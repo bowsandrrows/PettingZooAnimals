7980d5d62a32c530f1b38b47cc9b79971276b6f8
bowsandarrows <154934324+bowsandrrows@users.noreply.github.com>
2025-07-20 12:48:04 +0000

    added foreach loop

--- CsharpProject/PettingZooAnimals/Program.cs
+++ CsharpProject/PettingZooAnimals/Program.cs
@@ -6,13 +6,16 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
-
+foreach (var item in pettingZoo)
+{
+    System.Console.WriteLine(item + "is in the zoo");
+}
 // a pseudo-code method to randomize the petting zoo animals
 // RandomizeAnimals();
 
 // string[ , ] group = AssignGroup();
 
-Console.WriteLine("Shool A");
+Console.WriteLine("Shool A\n");
 
 
 // Example of how to assign animals to groups
