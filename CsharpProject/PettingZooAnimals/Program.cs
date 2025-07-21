ac1b801cbf69c1c486aa30bf3f42162bda41e7ba
bowsandarrows <154934324+bowsandrrows@users.noreply.github.com>
2025-07-21 12:43:29 +0000

    Refactor RandomizeAnimals method and remove foreach loop

--- CsharpProject/PettingZooAnimals/Program.cs
+++ CsharpProject/PettingZooAnimals/Program.cs
@@ -6,13 +6,22 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
-foreach (var item in pettingZoo)
-{
-    System.Console.WriteLine(item + " - is in the zoo");
-}
 // a pseudo-code method to randomize the petting zoo animals
-// RandomizeAnimals();
+void RandomizeAnimals()
+{
+    int i = 0;
+    int r = 1;
 
+    string temp = pettingZoo[i];
+    pettingZoo[i] = pettingZoo[r];
+    pettingZoo[r] = temp;
+
+    System.Console.WriteLine(pettingZoo[i] + " " + pettingZoo[r]);
+    System.Console.WriteLine(temp);
+
+}
+
+RandomizeAnimals();
 // string[ , ] group = AssignGroup();
 
 Console.WriteLine("Shool A\n");
